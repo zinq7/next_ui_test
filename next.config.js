f7eacfe7821cf99e38e8aca6efdb47a68f9d45eb
zinq <benfarmilo@gmail.com>
2024-09-08 18:05:58 -0700

    stuff

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
 }
 
 module.exports = nextConfig
